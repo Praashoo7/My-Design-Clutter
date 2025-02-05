4eb1fb10b5007f74b6b7318038ebf58c833a340e
Prashant <rnttl90@gmail.com>
2025-02-05 15:39:31 +0530

    updates&nfs

--- index.js
+++ index.js
@@ -28,21 +28,6 @@ document.querySelectorAll(".imageData").forEach((imgWrap) => {
     }
 });
 
-document.querySelectorAll(".imageDataUD").forEach((imgWrap) => {
-    const img = imgWrap.querySelector("img");
-
-    if (img.complete) {
-      imgWrap.style.setProperty("--before-opacity", "0");
-    } else {
-      img.addEventListener("load", () => {
-        imgWrap.style.setProperty("--before-opacity", "0");
-      });
-      img.addEventListener("error", () => {
-        console.error("Image failed to load:", img.src);
-      });
-    }
-});
-
 
 
 /* VIDEO-LOAD */
@@ -96,64 +81,32 @@ document.querySelectorAll('.imageData').forEach((imageData) => {
   });
 });
 
-document.querySelectorAll('.imageDataUD').forEach((imageData) => {
-  const img = imageData.querySelector('img');
-  const video = imageData.querySelector('.video');
-  const videoLoad = imageData.querySelector('.videoLoad');
 
-  let videoLoaded = false;
 
-  imageData.addEventListener('mouseenter', () => {
-    img.style.opacity = '0';
+/* INTERCHANGING-DIVS */
 
-    if (!videoLoaded) {
-      videoLoad.style.opacity = '1';
-      video.load();
+document.addEventListener("DOMContentLoaded", function () {
+  const wrappers = document.querySelectorAll(".wrapper");
 
-      video.addEventListener('canplay', () => {
-        if (!videoLoaded) {
-          videoLoaded = true;
-          video.style.width = "100.1%"
-          video.style.height = "100.1%"
-          videoLoad.style.width = "100.1%"
-          videoLoad.style.height = "100.1%"
-          video.style.opacity = '1';
-          setTimeout(() => {
-            videoLoad.style.opacity = '0';
-          }, 800);
-          video.play();
-        }
-      }, { once: true });
-    } else {
-      video.style.width = "100.1%"
-      video.style.height = "100.1%"
-      videoLoad.style.width = "100.1%"
-      videoLoad.style.height = "100.1%"
-      videoLoad.style.opacity = '0';
-      video.style.opacity = '1';
-      video.play();
-    }
-  });
+  wrappers.forEach((wrapper, index) => {
+      const textData = wrapper.querySelector(".textData");
+      const imageData = wrapper.querySelector(".imageData");
 
-  imageData.addEventListener('mouseleave', () => {
-    video.style.width = "99.5%"
-    video.style.height = "99.5%"
-    videoLoad.style.width = "99.5%"
-    videoLoad.style.height = "99.5%"
-    img.style.opacity = '1';
-    video.pause();
-    video.style.opacity = '0';
-    videoLoad.style.opacity = '1';
+      if (textData && imageData) {
+          const textContentDiv = textData.querySelector("div");
+
+          if (index % 2 === 0) {
+              wrapper.appendChild(imageData);
+              wrapper.appendChild(textData);
+
+              if (textContentDiv) textContentDiv.className = "data";
+          } else {
+              wrapper.appendChild(textData);
+              wrapper.appendChild(imageData);
+              imageData.style.transform = 'rotateZ(-1deg)';
+
+              if (textContentDiv) textContentDiv.className = "dataUD";
+          }
+      }
   });
 });
-
-
-
-/* NOTE */
-
-const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-if(isTouchDevice){
-  document.getElementById('note').innerHTML = 'PS : Tap on the Images to see the Actual site Design!'
-} else {
-  document.getElementById('note').innerHTML = 'PS : Hover over the Images to see the Actual site Design!'
-}
